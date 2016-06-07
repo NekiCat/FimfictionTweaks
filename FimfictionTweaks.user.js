135a8eb73c39e0127f78bfafa9a1bdc539309a67
TigeR <a.linck@onlinehome.de>
2016-06-08 01:01:32 +0200

    Added empty userscript

---
+++ FimfictionTweaks.user.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name Fimfiction Tweaks
+// @version 0.0.1
+// @description
+// @copyright 2016, TigeR
+// @license MIT, https://github.com/NekiCat/FimfictionTweaks/blob/master/LICENSE
+// @homepageURL https://github.com/NekiCat/FimfictionTweaks
+// ==/UserScript==
