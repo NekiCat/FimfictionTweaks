ac86ff2417f10a5c3a84ef94847f95a948a68b8c
TigeR <a.linck@onlinehome.de>
2016-06-08 14:47:25 +0200

    Added match to fimfiction.net in meta data

--- FimfictionTweaks.user.js
+++ FimfictionTweaks.user.js
@@ -1,8 +1,9 @@
 // ==UserScript==
 // @name Fimfiction Tweaks
 // @version 0.0.1
-// @description
+// @description Introduces small tweaks and improvements into fimfiction.net
 // @copyright 2016, TigeR
 // @license MIT, https://github.com/NekiCat/FimfictionTweaks/blob/master/LICENSE
 // @homepageURL https://github.com/NekiCat/FimfictionTweaks
+// @match *://*.fimfiction.net/
 // ==/UserScript==
