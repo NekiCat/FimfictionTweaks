078a66a42f34044ac9451324a39657d42a57dc0f
TigeR <a.linck@onlinehome.de>
2016-06-08 15:52:29 +0200

    Added function to open "hidden chapter" boxes that only contain a single chapter

--- FimfictionTweaks.user.js
+++ FimfictionTweaks.user.js
@@ -19,4 +19,15 @@
             return $('<div style="position: absolute; width: 100%; text-align: center; line-height: 14px; color: white; text-shadow: none; font-size: 0.75em;">' + Math.round($like[0].style.width.slice(0, -1) * 10) / 10 + '%</div>');
         }
     });
+
+    // If chapters are hidden, show all chapter blocks that only contain a single chapter
+    var $ce = $('.chapter_expander');
+    $ce.each(function(i) {
+        var t = $(this).children('li').text().trim();
+        t = t.substr(0, t.indexOf(' '));
+        if (t == '1') {
+            $(this).nextAll('.chapter_container_hidden').removeClass('chapter_container_hidden');
+            $(this).remove();
+        }
+    });
 })();
